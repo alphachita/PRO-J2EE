ac30f6dcc3ab279bcec81db71aa66f6cda69725d
pappukapgatelive <42207395+pappukapgatelive@users.noreply.github.com>
2019-05-16 12:29:55 +0530

    Add files via upload

---
+++ Day-13/spring-mvc-first-app/src/com/cdac/springmvcapp/HelloController.java
@@ -0,0 +1,17 @@
+package com.cdac.springmvcapp;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class HelloController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+			return new ModelAndView("success", "msg","Hello Spring MVC !!!!");
+	}
+	
+}
